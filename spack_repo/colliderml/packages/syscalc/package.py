34707fb77faceb3eeca9be4aa9ffe13268aa6c17
Paul Gessinger <hello@paulgessinger.com>
2025-09-10 17:13:35 +0200

    sw update

---
+++ spack_repo/colliderml/packages/syscalc/package.py
@@ -0,0 +1,39 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+
+from spack.package import *
+
+
+class Syscalc(CMakePackage):
+    """A tool to derive theoretical systematic uncertainties"""
+
+    homepage = "https://cp3.irmp.ucl.ac.be/projects/madgraph/wiki/SysCalc"
+    url = "https://bazaar.launchpad.net/~mgtools/mg5amcnlo/SysCalc/tarball/17"
+
+    # This is a mirror + added CMakeLists.txt file
+    git = "https://github.com/paulgessinger/SysCalc.git"
+
+    version(
+        "main",
+        branch="main"
+        # "1.1.7",
+        # commit="264e439566cc8204c8311a0d1658a785b28e2630"
+    )
+
+    depends_on("cxx", type="build")  # generated
+    depends_on("fortran", type="build")  # generated
+
+    tags = ["hep"]
+
+    depends_on("lhapdf@6:")
+
+    def url_for_version(self, version):
+        url = self.url.rsplit("/", 1)[0]
+        url += "/SysCalc_V{0}.tar.gz"
+
+        url = url.format(version)
+        return url
+
