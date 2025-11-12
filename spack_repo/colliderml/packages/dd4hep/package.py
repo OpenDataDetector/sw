3e4b024b99e9e7956d87a8d0df45e224b37e65da
Paul Gessinger <hello@paulgessinger.com>
2025-11-12 16:34:21 +0900

    update configuration

--- spack_repo/colliderml/packages/dd4hep/package.py
+++ spack_repo/colliderml/packages/dd4hep/package.py
@@ -8,10 +8,6 @@ from spack_repo.builtin.packages.dd4hep.package import Dd4hep as DD4hepBase
 from spack.package import *
 
 
-# Inherit everything from builtin.dd4hep, but override the mt patch we need
+# Inherit everything from builtin.dd4hep, but override the source repository
 class Dd4hep(DD4hepBase):
     git = "https://github.com/murnanedaniel/DD4hep.git"
-
-    version("develop", commit="a3fdbc4302494ee6dad3e3da460ec42b2b61c99f")
-
-    # patch("xfull_truth_particle_handler.patch", when="@1.32")
