ee9914326e90a2418de5ec1295cfb3d4433d613c
Paul Gessinger <hello@paulgessinger.com>
2025-11-13 10:47:19 +0900

    yaml file

--- ci/spack_build.sh
+++ ci/spack_build.sh
@@ -45,7 +45,7 @@ end_section
 echo "Spack version: $(spack --version)"
 
 start_section "Create environment"
-spack env create -d . "$SCRIPT_DIR"/spack.yaml 
+spack env create -d . "$SCRIPT_DIR/../spack.yaml"
 # spack -e . mirror set --autopush acts-spack-buildcache
 end_section
 
